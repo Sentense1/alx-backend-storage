51829ba69ab99fec1688566f613f7e1b3415d72b
sentense1 <3xsaint@gmail.com>
2023-10-12 10:56:12 +0000

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- Creates 'users' table if table is not existing
+-- Creates 'users' table if table is not existing.
 CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VAR(255)
+	name VAR(255),
+	PRIMARY KEY (id)
 );
