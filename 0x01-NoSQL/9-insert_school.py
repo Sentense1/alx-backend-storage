1a3ec15361f1d73cb466dc9821ea122cd0baf873
sentense1 <3xsaint@gmail.com>
2023-10-16 04:46:55 -0700

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -20,15 +20,16 @@ def insert_school(mongo_collection, **kwargs):
     # Return the captured inserted ID.
     return insertions.inserted_id
 
+
 if __name__ == "__main__":
     list_all = __import__('8-all').list_all
-
-
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
-    new_school_id = insert_school(school_collection, name="UCSF", address="505 Parnassus Ave")
+    new_school_id = insert_school(school_collection, name="UCSF",
+                                  address="505 Parnassus Ave")
     print("New school created: {}".format(new_school_id))
 
     schools = list_all(school_collection)
     for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('address', "")))
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'),
+                                  school.get('address', "")))
