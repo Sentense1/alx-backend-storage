f37b22625310ba07fab92b7c5df7d0fc35864279
sentense1 <3xsaint@gmail.com>
2023-10-20 13:49:29 +0000

    refactored

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,37 @@
 #!/usr/bin/env python3
-""" Module to make requests to a site and save to Redis """
-
-from functools import wraps
+"""
+Caching request module
+"""
 import redis
 import requests
+from functools import wraps
 from typing import Callable
 
 
-def count_requests(function: Callable) -> Callable:
-    """Decorator for counting requests and caching responses in Redis"""
-
-    @wraps(function)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """Wrapper for decorator"""
-        # Create a Redis connection
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
         client = redis.Redis()
-
-        count_url_key = f"count:{url}"
-        client.incr(count_url_key)
-        Count = client.get(count_url_key)
-        # print out url count
-        print("{} was requested {} times\n\n".format(url, Count))
-        cached_url_key = f"cached:{url}"
-        cached_html = client.get(cached_url_key)
-
-        if cached_html:
-            return cached_html.decode('utf-8')
-        else:
-            html = function(url)
-            client.setex(cached_url_key, 10, html)
-            return html
-
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
 
-@count_requests
+@track_get_page
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a URL using requests"""
+    """ Makes a http request to a given endpoint
+    """
     response = requests.get(url)
     return response.text
+
