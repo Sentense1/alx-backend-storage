47b847bce56d7eba81748e25b870de3c26217357
sentense1 <3xsaint@gmail.com>
2023-10-21 07:42:08 +0000

    reformed all

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,44 +1,51 @@
 #!/usr/bin/env python3
-""" Module to make requests to a site and save to Redis """
-
-import functools
+"""web module
+"""
+from functools import wraps
+from typing import Callable
 import redis
 import requests
-from typing import Callable
+
+_redis = redis.Redis()
+_redis.flushdb()
 
 
-def count_requests(function: Callable) -> Callable:
-    """Decorator for counting requests and caching responses in Redis"""
+def count_requests(method: Callable) -> Callable:
+    """count_requests function
 
-    @functools.wraps(function)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """Wrapper for decorator"""
-        # Create a Redis connection
-        client = redis.Redis()
-        client.flushdb()
+    Args:
+        method (Callable): method
 
-        count_url_key = f"count:{url}"
-        client.incr(count_url_key)
-        Count = client.get(count_url_key)
-        # print out url count
-        print("{} was requested {} times\n\n".format(url, Count))
-        cached_url_key = f"cached:{url}"
-        cached_html = client.get(cached_url_key)
+    Returns:
+        Callable: wrapper
+    """
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """wrapper function
 
-        if cached_html:
-            html = cached_html.decode("utf-8")
-            client.delete(cached_url_key)
-            return html
-        else:
-            html = function(url)
-            client.set(cached_url_key, html, 10)
-            return html
-
+        Returns:
+            [type]: wrapper
+        """
+        url = args[0]
+        cached = _redis.get(f"cached:{url}")
+        if cached:
+            return cached.decode("utf-8")
+        response = method(*args, **kwargs)
+        _redis.incr(f"count:{url}")
+        _redis.setex(f"cached:{url}", 10, response)
+        return response
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a URL using requests"""
-    response = requests.get(url)
+    """get_page function
+
+    Args:
+        url (str): url
+
+    Returns:
+        str: response
+    """
+    response = requests.get(url, timeout=10)
     return response.text
