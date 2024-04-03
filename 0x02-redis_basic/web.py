a1e2cfb53837f360cc2878dfb8d6f94f6c9d998a
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-03 06:52:00 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,24 +2,63 @@
 """
 Module for making request for Redis
 """
-import redis
 import requests
+from functools import wraps
+from redis import Redis
+from typing import Callable, Optional
 
+# Redis connection details (adjust if needed)
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+# Cache expiration time in seconds
+CACHE_EXPIRY = 10
 
+# Redis client
+redis_client = Redis(host=REDIS_HOST, port=REDIS_PORT)
 
-def get_page(url: str) -> str:
+def access_count(func: Callable) -> Optional[Callable]:
     """
-    Get_page function
+    Function for wrapping get_page
     """
-    """obtain the HTML content of a particular URL and returns it.
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Optional[str]:
+        """
+        FUnction to cache the number of times a url is accessed
+        """
+        cache_key = f"count:{args[0]}"
+
+        url_text, status_code = func(*args, **kwargs)
+        print('url test: ', url_text, 'status code: ', status_code)
+        if url_text and status_code == 200:
+            redis_client.incr(cache_key)
+            redis_client.expire(cache_key, CACHE_EXPIRY)
+            value = redis_client.get(cache_key)
+            return value
+        else:
+            print("Request to url failed")
+            return
+
+    return wrapper
+
+@access_count
+def get_page(url: str) -> Optional[tuple]:
     """
-    response = requests.get(url)
-    html_content = response.text
-    cached = r.get(f"cached{html_content}")
-    if cached:
-        return cached.decode("utf-8")
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, html_content)
-    return html_content
+    Fetches the HTML content of a URL using requests.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        str: The HTML content of the URL.
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except Exception:
+        pass
+    return response.text, response.status_code
+
+
+if __name__ == "__main__":
+    pass
