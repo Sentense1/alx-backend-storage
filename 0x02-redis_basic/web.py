e06eb7dbf5d6e8f62b574f17a5b12c28bc7e7515
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-03 07:31:59 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,19 +4,41 @@ Module for making request for Redis
 """
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 
+def counter(func: Callable) -> Callable:
+    """
+    FUnction wrapper, outer
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> None:
+        """
+        MAin function wrapper
+        """
+        url = args[0]
+        access_count = f"cahed:{url}"
+        r.incr(access_count)
+        r.expire(access_count, 10)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@counter
 def get_page(url: str) -> str:
     """
-    Get_page function
-    obtain the HTML content of a particular URL and returns it.
+    Fetches the HTML content of a URL using requests.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        str: The HTML content of the URL.
     """
     response = requests.get(url)
     html_content = response.text
-    if html_content and response.status_code == 200:
-        r.incr(f"count:{url}")
-        r.expire(f"cached:{url}", 10)
     return html_content
