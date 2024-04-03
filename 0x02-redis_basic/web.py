9bc2684195fe04ccd8e6cd7d59839ac5cfdbb830
Johnson Oragui <johnson.oragui@gmail.com>
2024-04-03 07:08:35 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,61 +2,21 @@
 """
 Module for making request for Redis
 """
+import redis
 import requests
-from functools import wraps
-from redis import Redis
-from typing import Callable, Optional
 
-# Redis connection details (adjust if needed)
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
 
-# Cache expiration time in seconds
-CACHE_EXPIRY = 10
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-# Redis client
-redis_client = Redis(host=REDIS_HOST, port=REDIS_PORT)
 
-def access_count(func: Callable) -> Optional[Callable]:
-    """
-    Function for wrapping get_page
-    """
-    @wraps(func)
-    def wrapper(*args, **kwargs) -> Optional[str]:
-        """
-        FUnction to cache the number of times a url is accessed
-        """
-        cache_key = f"count:{args[0]}"
-
-        url_text = func(*args, **kwargs)
-        if url_text:
-            redis_client.incr(cache_key)
-            redis_client.expire(cache_key, CACHE_EXPIRY)
-            value = redis_client.get(cache_key)
-            return value
-        else:
-            return
-
-    return wrapper
-
-@access_count
 def get_page(url: str) -> str:
     """
-    Fetches the HTML content of a URL using requests.
-
-    Args:
-        url (str): The URL to fetch.
-
-    Returns:
-        str: The HTML content of the URL.
+    Get_page function
+    obtain the HTML content of a particular URL and returns it.
     """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except Exception:
-        pass
-    return response.text
-
-
-if __name__ == "__main__":
-    pass
+    response = requests.get(url)
+    html_content = response.text
+    if html_content and response.status_code == 200:
+        r.incr(f"count:{url}")
+        r.expire(f"cached:{url}", 10)
+    return html_content
